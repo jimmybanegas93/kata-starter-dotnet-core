2907f5fadea620b07caad3a16147944babbd6358
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-06 15:19:01 -0600

    Two numbers passing, testing for unknown amount of numbers

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -58,10 +58,23 @@ namespace Kata.Spec
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_input_is_an_unknown_amount_of_numbers
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3,4"); };
+
+        It should_return_the_sum_of_all_those_numbers = () => { _result.Should().Be(10); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
 }
 
 
-// 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
