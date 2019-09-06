6562b2fd5f6335a28b457b0729e1a30b3bc76a14
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-06 15:42:19 -0600

    One negative passing, testing for multiple negatives

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -20,6 +20,12 @@ namespace Kata
             }
 
             var numbers = newUserInput.Split(delimiters, StringSplitOptions.None).Select(int.Parse).ToArray();
+            var negatives = numbers.Where(n => n < 0).ToArray();
+
+            if (negatives.Any())
+            {
+                throw new Exception($"negatives not allowed: {negatives.First()}");
+            }
 
             return numbers.Sum();
         }
