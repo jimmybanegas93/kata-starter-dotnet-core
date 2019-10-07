3c4e15f826e2778e4afb21b09673802c50e992d5
Ferenck Vargas <ferenck@acklenavenue.com>
2019-10-07 12:00:55 -0600

    Numbers greater than 1000 passing, testing for one multi character delimiter

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -24,6 +24,7 @@ namespace Kata
             var numbers = newUserInput
                 .Split(delimiters, StringSplitOptions.None)
                 .Select(int.Parse)
+                .Where(n => n <=1000)
                 .ToArray();
 
             var negatives = numbers.Where(n => n < 0).ToArray();
