96d545a83106a2a1bcb7587fa6b0bb7fefdb4723
Ferenck Vargas <ferenck@acklenavenue.com>
2019-10-14 11:29:41 -0600

    One negatives passing, multiple negatives failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -26,6 +26,12 @@ namespace Kata
                 .Select(int.Parse)
                 .ToArray();
 
+            var negatives = numbers.Where(n => n < 0).ToArray();
+            if (negatives.Any())
+            {
+                throw new Exception($"negatives not allowed: {negatives.First()}");
+            }
+
             return numbers.Sum();
         }
     }
