a511e479e82d51893ad33f928dbc33a7522bfbf6
Jimmy Ramos <jimmy.ramos@unitec.edu>
2019-10-14 11:38:05 -0600

    single custom delimiters passing, multiple custom delimiters test

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -16,7 +16,7 @@ namespace Kata
             if (userInput.StartsWith("//"))
             {
                 var splittedParts = userInput.Split("\n");
-                delimiters = new string[] {splittedParts.First().Replace("//", "")};
+                delimiters = new string[] {splittedParts.First().Replace("//", "").Replace("[", "").Replace("]","")};
 
                 newUserInput = splittedParts.Last();
             }
@@ -24,14 +24,11 @@ namespace Kata
 
             var numbers = newUserInput.Split(delimiters, StringSplitOptions.None)
                 .Select(int.Parse)
-                .Where(n=>n<=1000)
+                .Where(n => n <= 1000)
                 .ToArray();
 
             var negatives = numbers.Where(n => n < 0).ToArray();
-            if (negatives.Any())
-            {
-                throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
-            }
+            if (negatives.Any()) throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
 
             return numbers.Sum();
         }
