5e33ad38c38d611da3a83dd7947ecc23a9c8bd61
Ferenck Vargas <ferenck@acklenavenue.com>
2019-10-14 11:19:33 -0600

    New line as delimiter passing, testing for custom delimiter

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,7 +10,7 @@ namespace Kata
             if(string.IsNullOrEmpty(userInput))
                 return 0;
 
-            var numbers = userInput.Split(",")
+            var numbers = userInput.Split(new []{",", "\n"}, StringSplitOptions.None)
                 .Select(int.Parse)
                 .ToArray();
 
