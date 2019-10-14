0f7187324fb9da92503a1f85a473d54084753138
Jimmy Ramos <jimmy.ramos@unitec.edu>
2019-10-14 11:03:40 -0600

    init, user input is empty test

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -6,15 +6,34 @@ namespace Kata.Spec
     public class when_feeding_the_monkey
     {
         static Monkey _systemUnderTest;
-        
-        Establish context = () => 
+
+        Establish context = () =>
             _systemUnderTest = new Monkey();
 
-        Because of = () => 
+        Because of = () =>
             _systemUnderTest.Eat("banana");
 
         It should_have_the_food_in_its_belly = () =>
             _systemUnderTest.Belly.Should().Contain("banana");
+    }
+
+    public class when_the_user_input_is_empty
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add(); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
+    public class Calculator
+    {
+        public int Add()
+        {
+            return -1;
+        }
     }
 }
 
@@ -29,4 +48,3 @@ namespace Kata.Spec
 // 9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
 // 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
 // 11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
-
