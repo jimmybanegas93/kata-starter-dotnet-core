8b23df8533c3ee3298bde3007bc6b456867e446e
Ferenck Vargas <ferenck@acklenavenue.com>
2019-10-29 14:39:14 -0600

    One number passing. Testing for two numbers

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -38,15 +38,28 @@ namespace Kata.Spec
             _systemUnderTest = new Calculator();
         };
 
-        Because of = () => { _result = _systemUnderTest.Add(""); };
+        Because of = () => { _result = _systemUnderTest.Add("3"); };
 
         It should_return_the_same_number = () => { _result.Should().Be(3); };
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_input_has_two_numbers
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("2,3"); };
+
+        It should_return_the_sum = () => { _result.Should().Be(5); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
 }
 
-// 2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
